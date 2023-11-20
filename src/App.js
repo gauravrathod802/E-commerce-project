9b05e2ef767a3abd42246e0946d5d6422c599cd0
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-11-20 20:19:19 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
