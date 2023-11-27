e2ad43e80397288f6491881cfdc47681908ca61a
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-11-27 23:52:23 +0530

    updates

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import "./Styles/HomePage.css"
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 function HomePage() {
+    const router=useNavigate()
     const [products, setProducts] = useState([]);
     console.log(products, "product")
 
@@ -26,7 +28,8 @@ function HomePage() {
             <h1>All Products</h1>
             <div className='homepage-box'>
                 {products.map((product) => (
-                    <div className='homepage-box-child'>
+                    <div className='homepage-box-child'
+                    onClick={()=>router('/single-product/:id')}>
                         <img src={product.image} alt='' />
                         <h3>{product.title}</h3>
                         <h4>${product.price}</h4>
